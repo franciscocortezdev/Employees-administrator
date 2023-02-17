b1d4567a12a9e9ec685307df93261a3019ca2d83
Francisco Cortez <franciscocortezdev@gmail.com>
2023-02-17 12:52:09 -0600

    Departamente functionality

--- PresentationLayer/frmDepartaments.Designer.cs
+++ PresentationLayer/frmDepartaments.Designer.cs
@@ -28,16 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtDepartamentName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvDepartaments = new System.Windows.Forms.DataGridView();
+            this.ErrorNotification = new System.Windows.Forms.ErrorProvider(this.components);
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.departamentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDepartaments)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorNotification)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departamentBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -46,37 +53,39 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(24, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.Size = new System.Drawing.Size(66, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Code:";
             // 
-            // textBox1
+            // txtCode
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(29, 61);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(190, 36);
-            this.textBox1.TabIndex = 1;
+            this.txtCode.Enabled = false;
+            this.txtCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCode.Location = new System.Drawing.Point(29, 61);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(190, 36);
+            this.txtCode.TabIndex = 1;
             // 
-            // button1
+            // btnAdd
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(29, 129);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 51);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(29, 129);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(142, 51);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // textBox2
+            // txtDepartamentName
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(248, 61);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(582, 36);
-            this.textBox2.TabIndex = 4;
+            this.txtDepartamentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDepartamentName.Location = new System.Drawing.Point(248, 61);
+            this.txtDepartamentName.Multiline = true;
+            this.txtDepartamentName.Name = "txtDepartamentName";
+            this.txtDepartamentName.Size = new System.Drawing.Size(582, 36);
+            this.txtDepartamentName.TabIndex = 2;
             // 
             // label2
             // 
@@ -84,67 +93,114 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(243, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.Size = new System.Drawing.Size(187, 25);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Departament Name:";
             // 
-            // button2
+            // btnModify
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(210, 129);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 51);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnModify.Enabled = false;
+            this.btnModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModify.Location = new System.Drawing.Point(193, 129);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(142, 51);
+            this.btnModify.TabIndex = 4;
+            this.btnModify.Text = "Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
             // 
-            // button3
+            // btnDelete
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(384, 129);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(142, 51);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(358, 129);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(142, 51);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
-            // button4
+            // btnCancel
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(558, 129);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(142, 51);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(527, 129);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(142, 51);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // dataGridView1
+            // dgvDepartaments
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(29, 227);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(801, 400);
-            this.dataGridView1.TabIndex = 8;
+            this.dgvDepartaments.AllowUserToAddRows = false;
+            this.dgvDepartaments.AllowUserToDeleteRows = false;
+            this.dgvDepartaments.AutoGenerateColumns = false;
+            this.dgvDepartaments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDepartaments.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn});
+            this.dgvDepartaments.DataSource = this.departamentBindingSource;
+            this.dgvDepartaments.Location = new System.Drawing.Point(29, 231);
+            this.dgvDepartaments.MultiSelect = false;
+            this.dgvDepartaments.Name = "dgvDepartaments";
+            this.dgvDepartaments.ReadOnly = true;
+            this.dgvDepartaments.RowHeadersWidth = 51;
+            this.dgvDepartaments.RowTemplate.Height = 24;
+            this.dgvDepartaments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDepartaments.Size = new System.Drawing.Size(801, 400);
+            this.dgvDepartaments.TabIndex = 8;
+            this.dgvDepartaments.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDepartaments_CellClick);
+            // 
+            // ErrorNotification
+            // 
+            this.ErrorNotification.ContainerControl = this;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // departamentBindingSource
+            // 
+            this.departamentBindingSource.DataSource = typeof(EmployeeAdministrator.BusinessLayer.Departament);
             // 
             // frmDepartaments
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(861, 661);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dgvDepartaments);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.txtDepartamentName);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtCode);
             this.Controls.Add(this.label1);
             this.Name = "frmDepartaments";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmDepartaments";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.frmDepartaments_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDepartaments)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorNotification)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departamentBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,13 +209,17 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtDepartamentName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgvDepartaments;
+        private System.Windows.Forms.ErrorProvider ErrorNotification;
+        private System.Windows.Forms.BindingSource departamentBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
     }
 }
--- PresentationLayer/frmDepartaments.cs
+++ PresentationLayer/frmDepartaments.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeAdministrator.BusinessLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,11 +13,105 @@ namespace EmployeeAdministrator.PresentationLayer
 {
     public partial class frmDepartaments : Form
     {
+        Departament _businessLayerDepartament;
         public frmDepartaments()
         {
             InitializeComponent();
+            _businessLayerDepartament= new Departament();
         }
 
-        
+        #region Events of Form
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtDepartamentName.Text == string.Empty)
+            {
+                ErrorNotification.SetError(txtDepartamentName, "Empty data is not allowed");
+            }
+            else
+            {
+                var newDepartament = getDataForm();
+                _businessLayerDepartament.InsertDepartaments(newDepartament);
+                PopulateDataGrid();
+                CleanForm();
+            }
+        }
+
+        private void frmDepartaments_Load(object sender, EventArgs e)
+        {
+            PopulateDataGrid();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var DepartamentSelected = getDataForm();
+            _businessLayerDepartament.DeleteDepartaments(DepartamentSelected);
+            PopulateDataGrid();
+            CleanForm();
+            SaveMode();
+
+        }
+
+        private void dgvDepartaments_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtCode.Text = dgvDepartaments.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtDepartamentName.Text = dgvDepartaments.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+            EditMode();
+        }
+     
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            SaveMode();
+            CleanForm();
+        }
+
+        #endregion
+
+
+
+        private void PopulateDataGrid()
+        {
+            List<Departament> ListDepartaments = _businessLayerDepartament.GetDepartaments();
+            dgvDepartaments.DataSource = ListDepartaments;
+        }
+
+        private Departament getDataForm()
+        {
+            Departament newDepartament = new Departament();
+            newDepartament.Id = txtCode.Text != string.Empty? int.Parse(txtCode.Text): default;
+            newDepartament.Name = txtDepartamentName.Text;
+            return newDepartament;
+        }
+
+        private void EditMode()
+        {
+            btnAdd.Enabled = false;
+            btnModify.Enabled = true;
+            btnDelete.Enabled = true;
+            btnCancel.Enabled = true;
+        }
+        private void SaveMode()
+        {
+            btnAdd.Enabled = true;
+            btnModify.Enabled = false;
+            btnDelete.Enabled = false;
+            btnCancel.Enabled = false;
+        }
+
+        private void CleanForm()
+        {
+            txtCode.Text = string.Empty;
+            txtDepartamentName.Text = string.Empty;
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            var newDepartament = getDataForm();
+            _businessLayerDepartament.UpdateDepartaments(newDepartament);
+            PopulateDataGrid();
+            CleanForm();
+            SaveMode();
+        }
     }
 }
