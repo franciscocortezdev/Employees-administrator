fd9a85b10739f2ec00043b78f2545e930821024e
Francisco Cortez <franciscocortezdev@gmail.com>
2023-02-17 12:11:47 -0600

    Model Departament

---
+++ BusinessLayer/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAdministrator.BusinessLayer
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public int Departament { get; set; }
+        public byte[] Photo { get; set; }
+
+    }
+}
