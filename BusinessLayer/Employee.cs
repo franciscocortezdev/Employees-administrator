3d2862266cd16640f7343fee555346ae5cc55aa6
Francisco Cortez <franciscocortezdev@gmail.com>
2023-02-19 19:44:31 -0600

    Model Employee

--- BusinessLayer/Employee.cs
+++ BusinessLayer/Employee.cs
@@ -8,13 +8,37 @@ namespace EmployeeAdministrator.BusinessLayer
 {
     public class Employee
     {
+        DataLayer.Employee _dataLayerEmployee;
+
         public int Id { get; set; }
         public string Name { get; set; }
-        public string FirstName { get; set; }
-        public string SecondName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public int Departament { get; set; }
         public byte[] Photo { get; set; }
 
+        public Employee()
+        {
+            _dataLayerEmployee = new DataLayer.Employee();
+        }
+
+
+        public List<Employee> GetEmployees()
+        {
+            return _dataLayerEmployee.GetEmployees();
+        }
+
+        public void InsertDepartaments(Employee employee)
+        {
+            _dataLayerEmployee.InsertEmployee(employee);
+        }
+        public void UpdateDepartaments(Employee employee)
+        {
+            _dataLayerEmployee.UpdateEmployee(employee);
+        }
+        public void DeleteDepartaments(Employee employee)
+        {
+            _dataLayerEmployee.DeleteEmployee(employee);
+        }
     }
 }
