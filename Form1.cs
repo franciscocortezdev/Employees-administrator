39d8a2563e1b94ceb39e23acb851b7b9c84f3fc5
Francisco Cortez <franciscocortezdev@gmail.com>
2023-02-20 21:39:25 -0600

    Main manu

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeAdministrator.PresentationLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,27 @@ namespace EmployeeAdministrator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmDepartaments departaments= new frmDepartaments();
+            departaments.ShowDialog();
+            this.Show();
+        }
+
+        private void btnEmployees_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmEmployees employees = new frmEmployees();
+            employees.ShowDialog();
+            this.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
